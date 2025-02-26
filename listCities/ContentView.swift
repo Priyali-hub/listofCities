46bd4fe950176435497ade5396fabd71d787cf92
Priyali <PSrivastava16@slb.com>
2025-02-26 19:00:30 +0530

    Initial Commit

---
+++ listCities/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  listCities
+//
+//  Created by priyali.srivastava on 26/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ listCities/listCitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  listCitiesApp.swift
+//  listCities
+//
+//  Created by priyali.srivastava on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct listCitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
